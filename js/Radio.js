8528c83bc5f78af75d7d98e64d4e3259abf18b67
Victor Ribeiro <victorqribeiro@gmail.com>
2021-04-19 14:54:17 -0300

    add new station

--- js/Radio.js
+++ js/Radio.js
@@ -2,13 +2,10 @@ class Radio {
 
 	constructor(){
 		this.player = new Audio();
-		this.playlist = [/*{name: 'Box UK', slug: 'boxuk', src: 'http://192.227.85.169:6191/stream'},*/
-										 /*{name: 'Classic Rock Florida HD', slug: 'classicrockflorida', src: 'http://198.58.98.83:8258/stream'},*/
-										 /*{name: 'She Radio', slug: 'sheradio', src: 'http://airspectrum.cdnstream1.com:8136/1139_128'},*/
-										 /*{name: 'Rock And Roll Channel', slug: 'rockandrollchannel', src: 'http://uk2.internet-radio.com:8054/stream'},*/
-										 {name: 'Nightwave Plaza', slug: 'nightwave', src: 'https://radio.plaza.one/mp3'},
-				 						 /*{name: 'Brokenbeats', slug: 'brokenbeats', src: 'https://stream.brokenbeats.net/tune'}*/
-										];
+		this.playlist = [
+      {name: 'Nightwave Plaza', slug: 'nightwave', src: 'https://radio.plaza.one/mp3'},
+      {name: 'Classic Rock Florida HD', slug: 'classicrockflorida', src: 'https://vip2.fastcast4u.com/proxy/classicrockdoug?mp=/1'}
+		];
 		this.player.src = this.playlist[0].src;
 		this.player.preload = 'auto';
 		this.player.crossOrigin = 'anonymous';
